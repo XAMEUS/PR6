cc6192febca56c08a5f618d5fa5d120f6ad686c8
Quentin <quentinpn11@gmail.com>
2016-04-29 15:42:58 +0200

    first entity

---
+++ ringoProtocolJava/LoopService.java
@@ -0,0 +1,17 @@
+
+public class LoopService implements Runnable {
+
+	private Entity en;
+	
+	public LoopService(Entity en_p){
+		en = en_p;
+	}
+	
+	@Override
+	public void run() {
+		while(true){
+			//UDP PROTOCOL
+		}
+	}
+
+}
---
+++ ringoProtocolJava/WelcomeService.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class WelcomeService implements Runnable{
+	
+	private Entity en;
+
+	public WelcomeService(Entity en_p){
+		en=en_p;
+	}
+	
+	@Override
+	public void run() {
+		try{
+    		ServerSocket server=new ServerSocket(en.port_tcp);
+    		System.out.println("tcp "+en.port_tcp);
+    		while(true){
+	    		Socket socket=server.accept();
+	    		BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    		PrintWriter pw=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+	    		String welc = "WELC "+en.next_str+" "+en.multicast_str+"\n";
+	    		pw.print(welc);
+	    		pw.flush();
+	    		String newc = br.readLine();
+	    		if(newc.substring(0, 4).equals("NEWC")){
+	    			en.setNext(InetAddress.getByName(newc.substring(6,21)),Integer.parseInt(newc.substring(22)));
+	    		}else{
+	    			//TODO ERREUR
+	    		}
+	    		pw.print("ACKC\n");
+	    		pw.flush();
+	    		br.close();
+	    		pw.close();
+	    		socket.close();
+    		}    		
+    	}catch(Exception e){
+    		System.out.println(e);
+    		e.printStackTrace();
+    	}
+		
+	}
+
+}
